15a66d26063b7839b39d23d0e6d43eeab21377f8
burzamiszcz <burzynskihubert@gmail.com>
2021-04-22 17:09:18 +0200

    naprawa

--- user-microservice/app.py
+++ user-microservice/app.py
@@ -13,6 +13,18 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
 
 
+@app.route('/user_table', methods = ['GET'])
+def user_table():
+    x = {}
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute("SELECT * FROM Users")
+    i = 1
+    res=cur.fetchall()
+    for row in res:
+        x[i] = row
+        i+=1
+    return jsonify(x)
+
 @app.route('/new_user', methods=['POST'])
 def new_user():
     username = request.json['username']
@@ -30,36 +42,36 @@ def new_user():
     else:
         return jsonify({"status": "exist"})
 
-
-@app.route('/user_table', methods = ['GET'])
-def user_table():
-    x = {}
-    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    cur.execute("SELECT * FROM Users")
-    i = 1
-    res=cur.fetchall()
-    for row in res:
-        x[i] = row
-        i+=1
-    return jsonify(x)
-
-
-@app.route('/user_stats', methods = ['GET'])
-def user_stats():
-    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    cur.execute("SELECT COUNT(username) FROM Users")
-    res=cur.fetchall()    
-    return jsonify({'number_of_users': res[0]['COUNT(username)']})
-
-
 @app.route('/user_delete', methods=['POST'])
 def user_delete():
     id = request.json['id']
-    print(id, flush=True)
     cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
     cur.execute("DELETE FROM Users WHERE id=%s"%id)
     mysql.connection.commit() 
     return jsonify({'status': 'deleted'})
+
+@app.route('/user_change_name', methods=['POST'])
+def user_change_name():
+    username = request.json['new_username']
+    id = request.json['id']
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute("SELECT * FROM Users WHERE username=%s",(username.lower(),))
+    user = cur.fetchone()
+    if user == None:
+        cur.execute("UPDATE Users SET username = \'%s\' WHERE id = %s"%(username, id))
+        mysql.connection.commit() 
+        return jsonify({'status': 'changed'})
+    else:
+        return jsonify({'status': 'exists'})
+
+@app.route('/user_change_password', methods=['POST'])
+def user_change_password():
+    password = request.json['new_password']
+    id = request.json['id']
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute("UPDATE Users SET password = \'%s\' WHERE id = %s"%(password, id))
+    mysql.connection.commit() 
+    return jsonify({'status': 'changed'})
 
 if __name__ == '__main__':
     app.run()
