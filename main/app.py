29ffb819c36e79a5aa6638d87efe3b7919c9dc4e
burzamiszcz <burzynskihubert@gmail.com>
2021-04-22 13:09:31 +0200

    male_poprawki

--- main/app.py
+++ main/app.py
@@ -15,9 +15,9 @@ from flask import (
 from flask_mysqldb import MySQL, MySQLdb
 from flask_restful import reqparse, abort, Api, Resource
 
-
 app = Flask(__name__)
 app.secret_key = 'somesecretkeythatonlyishouldknow'
+
 
 @app.before_request
 def before_request():
@@ -28,31 +28,28 @@ def before_request():
     else:
         g.credential = None
         
+
 @app.route('/')
 def main():
     return redirect(url_for('login'))
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     session.clear()
     if request.method == 'POST':
         session.pop('credential', None)
-
         username = request.form['username'].lower()
         password = request.form['password']
-
         respose = requests.post("http://login:11000/", json = {'username': username, "password": password})
-
         if respose.json()['credential'] != None:
             session['credential'] = respose.json()['credential']
             session['id'] = respose.json()['id']
             session['username'] = respose.json()['username']
             return redirect(url_for('menu'))
-
         flash("Błedny login lub hasło")
-        #return redirect(url_for('login'))
-
     return render_template('login.html')
+
 
 @app.route('/menu')
 def menu():
@@ -60,27 +57,26 @@ def menu():
         return redirect(url_for('login'))
     return render_template('menu.html')
 
+
 @app.route('/users', methods=['GET', 'POST'])
 def menu_users():
     if g.credential == 'user':
         return redirect(url_for('login'))
 
-        
     if request.method == 'POST':
         id = request.form['id']
         if id != str(g.id):
             print(type(id), type(g.id), flush=True)
             response_id = requests.post("http://user:12000/user_delete", json = {'id': id})
-
     response = requests.get("http://user:12000/user_table")
     list = []
     list_id = []
     response = response.json()
-
     for elem in response:
         list.append([response[str(elem)]['id'], response[str(elem)]['username'], response[str(elem)]['password'], response[str(elem)]['credential']])
         #list_id.append([response[str(elem)]['id']])
     return render_template('users.html', data = list, data2 = list_id)
+
 
 @app.route('/add_user', methods=['GET', 'POST'])
 def menu_add_user():
@@ -109,17 +105,21 @@ def menu_add_user():
                 flash("Pomyślnie utworzono użytkownika")
     return render_template('add_user.html')
 
+
 @app.route('/cameras')
 def cameras():
     return render_template('cameras.html')
+
 
 @app.route('/stats')
 def stats():
     if g.credential == 'user':
         return redirect(url_for('login'))
+
     users = requests.get("http://user:12000/user_stats")
     g.number_of_users = users.json()['number_of_users']
     return render_template('stats.html')
+
 
 @app.route('/myprofile')
 def myprofile():
--- user-microservice/app.py
+++ user-microservice/app.py
@@ -12,17 +12,16 @@ app.config['MYSQL_HOST'] = 'zpipwr2021.atthost24.pl'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
 
+
 @app.route('/new_user', methods=['POST'])
 def new_user():
     username = request.json['username']
     password = request.json['password']
     credential = request.json['credential'] 
-
     cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
     cur.execute("SELECT * FROM Users WHERE username=%s",(username.lower(),))
     user = cur.fetchone()
     cur.close()
-
     if user == None:
         cur = mysql.connection.cursor()
         cur.execute('INSERT INTO Users (username, password, credential) VALUES (%s, %s, %s)',(username, password, credential))    
@@ -30,6 +29,7 @@ def new_user():
         return jsonify({"status": "created"}) 
     else:
         return jsonify({"status": "exist"})
+
 
 @app.route('/user_table', methods = ['GET'])
 def user_table():
@@ -42,6 +42,7 @@ def user_table():
         x[i] = row
         i+=1
     return jsonify(x)
+
 
 @app.route('/user_stats', methods = ['GET'])
 def user_stats():
